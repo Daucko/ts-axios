e5096f7a432c43c544c7f24fbcfc79032022eeb1
Daucko <dauda.a.tijani@gmail.com>
2024-03-27 15:46:43 +0000

    crypto summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-
-export type Crypto = {
-  ath: number;
-  atl: number;
-  current_price: number;
-  id: string;
-  name: string;
-  symbol: string;
-  high_24: number;
-  low_24: number;
-};
+import CryptoSummary from "./components/CryptoSummary";
+import { Crypto } from "./Types";
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>();
@@ -26,7 +17,7 @@ function App() {
     <div className="App">
       {cryptos
         ? cryptos.map((crypto) => {
-            return <p>{crypto.name + " $" + crypto.current_price}</p>;
+            return <CryptoSummary crypto={crypto} />;
           })
         : null}
     </div>
---
+++ src/Types.tsx
@@ -0,0 +1,10 @@
+export type Crypto = {
+  ath: number;
+  atl: number;
+  current_price: number;
+  id: string;
+  name: string;
+  symbol: string;
+  high_24: number;
+  low_24: number;
+};
